6f60b1814d13f6c2e6b0cd2091dbce5c1f635da3
akashjindal15 <ajindal1512@gmail.com>
2025-09-28 01:48:17 +0530

    Adding some test cases for App.tsx file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders string calculator heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/string calculator/i);
+  expect(headingElement).toBeInTheDocument();
+});
+
+test('renders input textarea', () => {
+  render(<App />);
+  const textareaElement = screen.getByPlaceholderText(/enter any string with numbers/i);
+  expect(textareaElement).toBeInTheDocument();
+});
+
+test('renders calculate button', () => {
+  render(<App />);
+  const buttonElement = screen.getByText(/calculate sum/i);
+  expect(buttonElement).toBeInTheDocument();
 });
